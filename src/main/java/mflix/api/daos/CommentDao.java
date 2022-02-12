b71f3f2823beb84f0b658af3aecfc5f1db0d50f2
Salih Candir <salih@live.at>
2022-02-12 21:02:06 +0100

    feat: implement the method that enables the deletion of a user comment

--- src/main/java/mflix/api/daos/CommentDao.java
+++ src/main/java/mflix/api/daos/CommentDao.java
@@ -126,12 +126,15 @@ public class CommentDao extends AbstractMFlixDao {
      * @return true if successful deletes the comment.
      */
     public boolean deleteComment(String commentId, String email) {
-        // TODO> Ticket Delete Comments - Implement the method that enables the deletion of a user
-        // comment
-        // TIP: make sure to match only users that own the given commentId
+        Bson filter = Filters.and(Arrays.asList(
+                Filters.eq("_id", new ObjectId(commentId)),
+                Filters.eq("email", email)
+        ));
+        DeleteResult deleteResult = commentCollection.deleteOne(filter);
+
         // TODO> Ticket Handling Errors - Implement a try catch block to
         // handle a potential write exception when given a wrong commentId.
-        return false;
+        return deleteResult.getDeletedCount() >= 1;
     }
 
     /**
